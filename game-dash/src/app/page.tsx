9029d314e99ee3f0a5b5f1f9f6e401b7ef7a71c0
Christian Johnson <33044116+cjallday7@users.noreply.github.com>
2025-08-10 12:03:54 -0400

    Update page.tsx
    adding Recently Played component for Steam

--- game-dash/src/app/page.tsx
+++ game-dash/src/app/page.tsx
@@ -15,6 +15,7 @@ import HomeIcon from "@mui/icons-material/Home";
 import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
 import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
 import SettingsIcon from "@mui/icons-material/Settings";
+import RecentlyPlayed from "../components/RecentlyPlayed";
 
 const drawerWidth = 240;
 
@@ -95,29 +96,11 @@ export default function Dashboard() {
         </Typography>
 
         {/* Recently Played on Steam section */}
-        <Typography variant="h5" sx={{ marginBottom: 2 }}>
-          Recently Played on Steam
-        </Typography>
+        <RecentlyPlayed />
 
         {/* Recently Played on Xbox section */}
         <Typography variant="h5" sx={{ marginBottom: 2 }}>
           Recently Played on Xbox
-        </Typography>
-
-
-        <Typography sx={{ marginBottom: 2 }}>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
         </Typography>
       </Box>
     </Box>
