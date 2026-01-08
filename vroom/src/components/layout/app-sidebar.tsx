2b6d7aaf2e779943dfc3a890c07849bfcea03495
Christian Johnson <33044116+cjallday7@users.noreply.github.com>
2026-01-08 13:41:41 -0500

    Update app-sidebar.tsx
    added footer with dropdown menu for settings

--- vroom/src/components/layout/app-sidebar.tsx
+++ vroom/src/components/layout/app-sidebar.tsx
@@ -1,7 +1,7 @@
 // Sidebar Component for Navigation and Layout
 
 //placeholder icons
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+import { Calendar, Home, Inbox, Search, Settings, User, ChevronUp } from "lucide-react"
 
 
 import {
@@ -16,6 +16,13 @@ import {
   SidebarMenuButton,
   SidebarHeader,
 } from "@/components/ui/sidebar"
+
+import { 
+    DropdownMenu,
+    DropdownMenuContent, 
+    DropdownMenuItem, 
+    DropdownMenuTrigger 
+} from "../ui/dropdown-menu"
 
 //nav items
 const navItems = [
@@ -48,10 +55,29 @@ export function AppSidebar() {
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
-
-        <SidebarGroup />
       </SidebarContent>
-      <SidebarFooter />
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <SidebarMenuButton>
+                    <User className="w-4 h-4" /> @hypraktiv
+                    <ChevronUp className="ml-auto" />
+                  </SidebarMenuButton>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  side="top"
+                  className="w-[--radix-popper-anchor-width]"
+                >
+                  <DropdownMenuItem>
+                    <Settings className="mr-2 w-4 h-4" /> Settings
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
     </Sidebar>
   )
 }
