ed71217d6171e9d29789447b200ff5849e759e8b
Christian Johnson <33044116+cjallday7@users.noreply.github.com>
2026-01-16 10:09:48 -0500

    Task 1: Initialize ElasticToggle component file

---
+++ elastic-toggle/src/App.jsx
@@ -0,0 +1,12 @@
+import ElasticToggle from './components/ElasticToggle'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <ElasticToggle />
+    </div>
+  )
+}
+
+export default App
---
+++ elastic-toggle/src/components/ElasticToggle.jsx
@@ -0,0 +1,5 @@
+function ElasticToggle() {
+  return null
+}
+
+export default ElasticToggle
